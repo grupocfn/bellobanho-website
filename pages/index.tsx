35b5b8627edd9926e7e96019ed67c2fceac899c8
Leonardo Meira <leonardo@casadasfechaduras.br>
2022-10-12 19:34:37 -0300

    add minimal responsiveness and react-aos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,20 @@
+import { useEffect } from 'react';
 import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Image from 'next/image';
 import { AiOutlineDoubleRight } from 'react-icons/ai';
-import Map, { Marker } from 'react-map-gl';
+import AOS from 'aos';
 import Link from 'next/link';
+import "aos/dist/aos.css"
 
 const Home = () => {
+
+  useEffect(() => {
+    AOS.init({ duration: 1000, });
+  }, [])
+
   return (
     <>
       <Head>
@@ -23,7 +30,7 @@ const Home = () => {
           </Link>
           <div className={styles.mainItemWrapper1}>
             <div className={styles.mainItem1}>
-              <div className={styles.mainItemTitle}>
+              <div data-aos="fade-up"  className={styles.mainItemTitle}>
                 <h1>Bem-vindo à BelloBanho</h1>
                 <p><AiOutlineDoubleRight /> Nós fornecemos as melhores soluções em revestimentos e acabamentos para o seu projeto</p>
               </div>
@@ -31,37 +38,37 @@ const Home = () => {
           </div>
           <div className={styles.mainItemWrapper2}>
           <div className={styles.mainItem2}>
-              <div className={styles.mainItemTitle}>
+              <div data-aos="fade-up" className={styles.mainItemTitle} id="mainItem2">
                 <h1>Produtos de primeira linha para o seu ambiente</h1>
                 <p><AiOutlineDoubleRight /> Adicione sofisticação e modernidade ao seu ambiente doméstico e/ou profissional</p>
               </div>
             </div>
           </div>
-          <div className={styles.brands}>
+          <div data-aos="fade-up" className={styles.brands} id="brands">
             <div className={styles.brandsTitleWrapper}>
               
               <h1>Trabalhamos com os principais fornecedores do mercado</h1>
               <small style={{textAlign: 'left'}}>Qualidade, elegância e robustez</small>
             </div>
             <div className="row">
-              <Image className={styles.brandLogo} width={130} height={68.89} src="/brands/giragrill.webp" alt="logo"/>
-              <Image className={styles.brandLogo} width={130} height={68.89} src="/brands/portinari.webp" alt="logo"/>
-              <Image className={styles.brandLogo} width={130} height={68.89} src="/brands/rerthy.webp" alt="logo"/>
-              <Image className={styles.brandLogo} width={130} height={68.89} src="/brands/lorenzetti.webp" alt="logo"/>
-              <Image className={styles.brandLogo} width={130} height={68.89} src="/brands/ralolinear.webp" alt="logo"/>
-              <Image className={styles.brandLogo} width={130} height={68.89} src="/brands/tramontina.webp" alt="logo"/>
-              <Image className={styles.brandLogo} width={130} height={68.89} src="/brands/atlas.webp" alt="logo"/>
-              <Image className={styles.brandLogo} width={130} height={68.89} src="/brands/eliane.webp" alt="logo"/>
+              <Image className={styles.brandLogo} loading="eager" width={130} height={68.89} src="/brands/giragrill.webp" alt="logo"/>
+              <Image className={styles.brandLogo} loading="eager" width={130} height={68.89} src="/brands/portinari.webp" alt="logo"/>
+              <Image className={styles.brandLogo} loading="eager" width={130} height={68.89} src="/brands/rerthy.webp" alt="logo"/>
+              <Image className={styles.brandLogo} loading="eager" width={130} height={68.89} src="/brands/lorenzetti.webp" alt="logo"/>
+              <Image className={styles.brandLogo} loading="eager" width={130} height={68.89} src="/brands/ralolinear.webp" alt="logo"/>
+              <Image className={styles.brandLogo} loading="eager" width={130} height={68.89} src="/brands/tramontina.webp" alt="logo"/>
+              <Image className={styles.brandLogo} loading="eager" width={130} height={68.89} src="/brands/atlas.webp" alt="logo"/>
+              <Image className={styles.brandLogo} loading="eager" width={130} height={68.89} src="/brands/eliane.webp" alt="logo"/>
             </div>
 
             <div className="row">
-              <Image className={styles.brandLogo} width={130} height={68.89} src="/brands/perflex.webp" alt="logo"/>
-              <Image className={styles.brandLogo} width={130} height={68.89} src="/brands/biancogres.webp" alt="logo"/>
-              <Image className={styles.brandLogo} width={130} height={68.89} src="/brands/decortiles.webp" alt="logo"/>
-              <Image className={styles.brandLogo} width={130} height={68.89} src="/brands/docol.webp" alt="logo"/>
-              <Image className={styles.brandLogo} width={130} height={68.89} src="/brands/fabrimar.webp" alt="logo"/>
-              <Image className={styles.brandLogo} width={130} height={68.89} src="/brands/franke.webp" alt="logo"/>
-              <Image className={styles.brandLogo} width={130} height={68.89} src="/brands/deca.webp" alt="logo"/>
+              <Image className={styles.brandLogo} loading="eager" width={130} height={68.89} src="/brands/perflex.webp" alt="logo"/>
+              <Image className={styles.brandLogo} loading="eager" width={130} height={68.89} src="/brands/biancogres.webp" alt="logo"/>
+              <Image className={styles.brandLogo} loading="eager" width={130} height={68.89} src="/brands/decortiles.webp" alt="logo"/>
+              <Image className={styles.brandLogo} loading="eager" width={130} height={68.89} src="/brands/docol.webp" alt="logo"/>
+              <Image className={styles.brandLogo} loading="eager" width={130} height={68.89} src="/brands/fabrimar.webp" alt="logo"/>
+              <Image className={styles.brandLogo} loading="eager" width={130} height={68.89} src="/brands/franke.webp" alt="logo"/>
+              <Image className={styles.brandLogo} loading="eager" width={130} height={68.89} src="/brands/deca.webp" alt="logo"/>
             </div>
           </div>
           
