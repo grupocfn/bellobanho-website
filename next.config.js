18ffe6a4f642c8de93e1871ce73ab4908e94fc73
grupocfn <informatica@casadasfechaduras.com.br>
2022-10-12 19:41:48 -0300

    set domains in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['www.bellobanho.com.br', 'grupocfn-bellobanho.netlify.app']
+  }
 }
 
 module.exports = nextConfig
